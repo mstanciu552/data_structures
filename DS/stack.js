8901e88b8364005b657e93e774ee9c7fac59db58
mstanciu552 <mstanciu552@gmail.com>
2021-02-12 17:26:22 +0200

    added stack implementation

---
+++ DS/stack.js
@@ -0,0 +1,19 @@
+export default class Stack {
+    static #data;
+    constructor() {
+        this.data = [];
+    }
+
+    push(element) {
+        this.data.push(element);
+    }
+
+    pop() {
+        this.data.pop();
+    }
+
+    // TODO: Have to change this
+    getLast() {
+        return this.data[this.data.length - 1];
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import LinkedList from './DS/linked_list.js';
+import Stack from './DS/stack.js';
 
+// Linked List testing 
 let list = new LinkedList(1);
 list.setNext(2);
 
-console.log(`List head value: ${list.getData()}\nList next value: ${list.next.getData()}`);
+// console.log(`List head value: ${list.getData()}\nList next value: ${list.next.getData()}`);
+
+// console.log(`Type of list head-> ${{list.head}} : ${typeof list.head}\nType of list next->${{list.next}} : ${typeof list.next}`)
+
+// Stack testing
+let stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(`Stack elements: ${stack.getLast()}`);
+
+stack.pop();
+
+console.log(`Stack elements: ${stack.getLast()}`);
