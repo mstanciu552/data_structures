49983b4759915ea1bd3ef8e8003c138f28f374e8
root <root@DESKTOP-56SLELS.localdomain>
2021-02-13 20:31:12 +0200

    added queue class

---
+++ DS/queue.js
@@ -0,0 +1,24 @@
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+
+    getFirst() {
+        try {
+            const res = this.data[0];
+
+            // Change this to a hand-written algorithm
+            this.data.shift();
+
+            return res;
+        } catch {
+            return `Queue is empty!`;
+        }
+    }
+
+    add(data) {
+        this.data.push(data);
+    }
+}
+
+module.exports = { Queue };
--- index.js
+++ index.js
@@ -2,26 +2,16 @@
  * TODO: Set up testing instead...maybe with JEST
  * */
 
-import LinkedList from './DS/linked_list.js';
-import Stack from './DS/stack.js';
+// const LinkedList = require("./DS/linked_list");
+// const Stack = require("./DS/stack");
+const { Queue } = require("./DS/queue");
 
-// Linked List testing 
-let list = new LinkedList(1);
-list.setNext(2);
+// Queue Testing
+let queue = new Queue();
 
-// console.log(`List head value: ${list.getData()}\nList next value: ${list.next.getData()}`);
+queue.add(1);
+queue.add(2);
+queue.add(3);
 
-// console.log(`Type of list head-> ${{list.head}} : ${typeof list.head}\nType of list next->${{list.next}} : ${typeof list.next}`)
-
-// Stack testing
-let stack = new Stack();
-
-stack.push(1);
-stack.push(2);
-stack.push(3);
-
-console.log(`Stack elements: ${stack.getLast()}`);
-
-stack.pop();
-
-console.log(`Stack elements: ${stack.getLast()}`);
+console.log(`Queue first element: ${queue.getFirst()}`);
+console.log(`Queue first element: ${queue.getFirst()}`);
