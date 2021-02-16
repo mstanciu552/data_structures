cca27025768f8e0ebf550f9876ff9fff46f45ed7
mstanciu552 <mstanciu552@gmail.com>
2021-02-16 22:52:22 +0200

    added tree data structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Tree } = require('./DS/tree.js');
+
+let tree = new Tree(1)
+
+tree.addChild(new Tree(2));
+tree.addChild(new Tree(3));
+tree.addChild(new Tree(4));
+
+tree.getChildren()[0].addChild(new Tree(5));
+
+console.log(`${tree.root} + ${tree.children.map(ch => ch.root)}`);
+
+console.log(`${tree.children[0].children[0].root}`);
