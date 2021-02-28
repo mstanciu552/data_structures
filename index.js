c19e2af0adc51f596b337dd4aa614b25b0b67d4a
mstanciu552 <mstanciu552@gmail.com>
2021-02-28 22:05:39 +0200

    merging conflict

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { sudoku } = require('./ALGO/backtracking');
+// const { sudoku } = require('./ALGO/backtracking');
 
-sudoku();
+// sudoku();
+
+const { LinkedList } = require('./DS/linked_list.js');
+const { reverse_ll } = require('./ALGO/linked_list.js');
+
+let ll = LinkedList.convertArray([1, 2, 3, 4, 5]);
+console.log(reverse_ll(ll).getArray());
