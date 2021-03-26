c4a4b202c34d9424db16ed4f693fcc67cdd7a85b
mstanciu552 <mstanciu552@gmail.com>
2021-03-26 23:50:40 +0200

    changed heap implementation

--- DS/heap.js
+++ DS/heap.js
@@ -1,49 +1,14 @@
 class Heap {
-    constructor(arr, order = 0) {
-        this.arr = arr;
-        this.order = order;
-    }
-        
-    getLength() {
-        return this.arr.length;
-    }
-
-    findMin() {
-        let min = Math.max(...this.arr);
-
-        for (let i = 0; i < this.arr.length; i++) 
-            if (min > this.arr[i])
-                min = this.arr[i];
-        
-        return min;
-    }
-    findMax() {
-        let max = Math.min(...this.arr);
-
-        for (let i = 0; i < this.arr.length; i++)
-            if (max < this.arr[i])
-                max = this.arr[i];
-        return max;
-    }
-    
-    removeItem(element) {
-        if (this.arr.length >= 0) {
-            const index = this.arr.indexOf(element);
-            if(index !== -1) this.arr.splice(index, 1);
-        }
-    }
-
-    pop() {
-        let first = this.order === 0 ? this.findMin() : this.findMax();
-        
-        this.removeItem(first);
-
-        return first;
-    }
-
-    push(data) {
-        this.arr.push(data);
-    }
+  constructor(capacity, len) {
+    this.arr = [];
+    this.capacity = capacity;
+    this.len = len;
+  }
+  insert() {}
+  delete() {}
+  extractMin() {}
 }
 
-module.exports = { Heap }
+export class MinHeap extends Heap {}
+
+export class MaxHeap extends Heap {}
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { Set } = require('./DS/Set');
 
 let set = Set.convertToSet([1, 1, 2, 3, 4, 4]);
 
-console.log(set);
+console.log(set.arr);
