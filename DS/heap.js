e3f01bc57559760a409391633a90198d558cd205
mstanciu552 <mstanciu552@gmail.com>
2021-03-27 01:28:36 +0200

    added heap changes

--- DS/heap.js
+++ DS/heap.js
@@ -1,35 +1,41 @@
 class Heap {
     constructor(capacity) {
         if (this.constructor === Heap)
-            throw new TypeError('Abstract class Heap cannot be instanciated!')
-        this.arr = []
-        this.capacity = capacity
-        this.len = 0
+            throw new TypeError('Abstract class Heap cannot be instanciated!');
+        this.arr = [];
+        this.capacity = capacity;
+        this.len = 0;
+        this.balance();
     }
 
     balance() {}
 
     insert(val) {
-        this.len++
-        this.arr[this.len] = val
-        this.balance()
+        this.arr[this.len] = val;
+        this.len++;
+        this.balance();
     }
-    delete(val) {}
+    delete(val) {
+        let idx = this.arr.indexOf(val);
+        if (idx === -1) return;
+        this.arr.splice(idx, 1);
+    }
 }
-
-const fun = el => {}
 
 export class MinHeap extends Heap {
     constructor(capacity) {
-        super(capacity)
+        super(capacity);
     }
-    balance() {}
+    balance() {
+        // TODO Implement heap balancing algorithm
+        console.log('Balanced');
+    }
     extractMin() {}
 }
 
 export class MaxHeap extends Heap {
     constructor(capacity) {
-        super(capacity)
+        super(capacity);
     }
     balance() {}
     extractMax() {}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { MinHeap } from "./DS/heap.js";
+import { MinHeap } from './DS/heap.js';
 
 let mh = new MinHeap(10);
 mh.insert(11);
@@ -7,5 +7,11 @@ mh.insert(40);
 mh.insert(53);
 mh.insert(21);
 mh.insert(73);
+
+console.log(mh.arr);
+
+mh.delete(73);
+mh.delete(11);
+mh.delete(12);
 
 console.log(mh.arr);
